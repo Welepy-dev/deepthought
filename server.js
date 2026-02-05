8e6d7f15e2bfe959d2e4d5be541f62f3b7dac935
Welepy-dev <mwsilva.contato@gmail.com>
2026-02-05 08:10:27 -0600

    Server logic, here we create the server, configure connections and enable real time communication, lastly we  listen for anything happening on port 8081

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
+import { createServer } from "http"; //http server
+import { Server } from "socket.io"; //engine for real-time, two way talk
+
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+
+// Get the current file path and directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename); 
+
+const app = express(); //initialize express
+const httpServer = createServer(app); // create http server using express app/tell the http server to use app to handle requests
+const io = new Server(httpServer); //initialize socket.io server to handle real-time communication
+
+// Serve static files from the "public" directory
+app.use(express.static(join(__dirname, "public"))); // serve files from this dir when requested
+
+// Handle socket.io connections
+io.on('connection', (socket) => {
+		console.log('A user connected:', socket.id);
+
+		//socket -> utilizador que se conectou
+		//socket.id -> identificador da conexao
+
+		socket.on('disconnect', () => {
+				console.log('User disconnected:', socket.id);
+		});
+});
+
+const PORT = 8081;
+httpServer.listen(PORT, () => {
+	console.log(`Server running at http://localhost:${PORT}`);
+});
