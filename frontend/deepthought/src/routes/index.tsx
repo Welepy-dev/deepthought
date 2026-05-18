a731ee18e8c2a97846d5a50734da17ae66a3d69b
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-18 20:56:16 +0100

    index to track routes

---
+++ frontend/deepthought/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import App from "../App";
+import EmailSignIn from "../signin/email_signin";
+import SignIn from "../signin/signin";
+import OTPEmail from "../signin/OTPemail";
+
+export function AppRouter() {
+	return (
+			<Routes>
+				<Route path="/" element=<App CustomComponent={SignIn}/>/>
+				<Route path="/EmailSignIn" element=<App CustomComponent={EmailSignIn}/>/>
+				<Route path="/OTPEmail" element=<App CustomComponent={OTPEmail}/>/>
+
+			</Routes>
+
+	)
+}
