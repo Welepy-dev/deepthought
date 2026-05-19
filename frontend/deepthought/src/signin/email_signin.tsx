25424e4e0e09c7a1e08b29d1449b84266e6c0501
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-19 19:36:31 +0100

    some tweaks in the email login fomr

--- frontend/deepthought/src/signin/email_signin.tsx
+++ frontend/deepthought/src/signin/email_signin.tsx
@@ -1,16 +1,19 @@
 import emailIcon from '../assets/closed_email.png'
+import CustomButton from '../components/CustomButton'
 
 export default function EmailSignIn() {
+
 	return (
-			<div className="flex flex-col items-center w-1/2 xl:w-2/5 h-96 bg-neutral_contrast border-b-8 border-r-8 border-l-4 border-t-4 border-black">
+			<div className="flex flex-col items-center w-[500px]  h-96 bg-neutral_contrast border-b-8 border-r-8 border-l-4 border-t-4 border-black">
 				<div className='flex flex-col items-center'>
 					<img src={ emailIcon } alt="Email icon" className='w-24 h-auto'/>
 					<p className="font-pressStart text-contrast">
 						Email signin
 					</p>
 				</div>
-				<input className="border-b-8 border-r-8 border-l-4 border-t-4 border-black" type="poggers" />
-				<p className='font-pressStart text-contrast text-xs'>A code will be sent to your email.</p>
+				<input placeholder='johndoe@mail.com' className="text-center px-16 py-2 text-sm font-pressStart focus:outline-none border-b-8 mt-4 border-r-8 border-l-4 border-t-4 border-black" type="poggers" />
+				<p className='font-pressStart text-center px-6 text-contrast text-xs mt-4'>A code will be sent to your email.</p>
+				<CustomButton route='/OTPEmail' name='Send OTP code'></CustomButton>
 			</div>
 	)
 }
