f9e4f88ebcfc34dca706dcf74cb8095cfff66d56
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-19 19:37:42 +0100

    tweaks ant the sign in page

--- frontend/deepthought/src/App.tsx
+++ frontend/deepthought/src/App.tsx
@@ -1,13 +1,21 @@
+import "./App.css"
 
 export default function App({CustomComponent} : {CustomComponent : any}) {
-	console.log(CustomComponent)
-	return (
-		<>
-			<div className="bg-background h-screen">
-				<div className='h-screen flex items-center justify-center '>
-					<CustomComponent/>
-				</div>
-			</div>
-		</>
-	)
+  console.log(CustomComponent)
+  return (
+    <>
+      <div className="bg-background stars min-h-screen">
+        <div className='h-screen flex items-center justify-center'>
+          <CustomComponent/>
+        </div>
+      </div>
+
+      {/* CRT Filter Overlay */}
+      <div className="crt-overlay" aria-hidden="true">
+        <div className="crt-scanlines" />
+        <div className="crt-vignette" />
+        <div className="crt-flicker" />
+      </div>
+    </>
+  )
 }
