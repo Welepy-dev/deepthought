f7075789e69bd4cf9fdd1a0b015dd18930026072
Welepy-dev <mwsilva.contato@gmail.com>
2026-05-20 17:16:23 +0100

    fix formatting: adjust indentation in AppRouter component

--- frontend/deepthought/src/routes/index.tsx
+++ frontend/deepthought/src/routes/index.tsx
@@ -7,12 +7,11 @@ import PhaserGame from "../game/PhaserGame";
 
 export function AppRouter() {
 	return (
-			<Routes>
-				<Route path="/" element=<App CustomComponent={SignIn}/>/>
-				<Route path="/EmailSignIn" element=<App CustomComponent={EmailSignIn}/>/>
-				<Route path="/OTPEmail" element=<App CustomComponent={OTPEmail}/>/>
-				<Route path="/Game" element=<App CustomComponent={PhaserGame}/>/>
-			</Routes>
-
+		<Routes>
+			<Route path="/" element=<App CustomComponent={SignIn}/>/>
+			<Route path="/EmailSignIn" element=<App CustomComponent={EmailSignIn}/>/>
+			<Route path="/OTPEmail" element=<App CustomComponent={OTPEmail}/>/>
+			<Route path="/Game" element=<App CustomComponent={PhaserGame}/>/>
+		</Routes>
 	)
 }
