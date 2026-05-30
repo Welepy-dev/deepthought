e67e3f81bdf150336bb7b8d6a59b01309d54eb7f
11StarLord <diogowalter114@gmail.com>
2026-05-31 00:21:38 +0100

    Updating forty-two strategy

---
+++ backend/src/auth/strategies/forty-two.strategy.ts
@@ -0,0 +1,35 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-oauth2';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class FortyTwoStrategy extends PassportStrategy(
+  Strategy,
+  '42',
+) {
+  constructor(config: ConfigService) {
+    super({
+      authorizationURL: 'https://api.intra.42.fr/oauth/authorize',
+      tokenURL: 'https://api.intra.42.fr/oauth/token',
+
+      clientID: config.get('FORTYTWO_CLIENT_ID'),
+      clientSecret: config.get('FORTYTWO_CLIENT_SECRET'),
+
+      callbackURL: config.get('FORTYTWO_CALLBACK'),
+
+      scope: [],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: Function,
+  ) {
+    done(null, {
+      accessToken,
+    });
+  }
+}
