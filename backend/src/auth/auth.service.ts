e6ebd8bb6be1cb3cb7985cd710085017cd04d959
11StarLord <diogowalter114@gmail.com>
2026-05-31 02:42:45 +0100

    updating auth.service.ts and  schema

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+} from '@nestjs/common';
+
+import { JwtService } from '@nestjs/jwt';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login42(accessToken: string) {
+
+    const response = await fetch(
+      'https://api.intra.42.fr/v2/me',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    const profile = await response.json();
+
+    let user = await this.prisma.user.findUnique({
+      where: {
+        fortyTwoId: profile.id,
+      },
+    });
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: {
+          fortyTwoId: profile.id,
+          login: profile.login,
+          email: profile.email,
+          displayName: profile.displayname,
+          avatar: profile.image.link,
+        },
+      });
+    }
+
+    const payload = {
+      sub: user.id,
+    };
+
+    const access_token =
+      await this.jwtService.signAsync(payload);
+
+    return {
+      access_token,
+      user,
+    };
+  }
+}
