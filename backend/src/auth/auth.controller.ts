a05a8788635d2f93ef859f44c3ee92281e06f500
11StarLord <diogowalter114@gmail.com>
2026-05-31 02:04:40 +0100

    Updating controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+
+import { AuthGuard } from '@nestjs/passport';
+
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+
+  @Get('42/login')
+  @UseGuards(AuthGuard('42'))
+  async login() {}
+
+  @Get('42/callback')
+  @UseGuards(AuthGuard('42'))
+  async callback(@Req() req) {
+    return this.authService.login42(
+      req.user.accessToken,
+    );
+  }
+}
