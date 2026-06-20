26baa359a3a8459fe207d2b5077fd1d1e529eb9d
11StarLord <diogowalter114@gmail.com>
2026-06-20 19:04:31 +0100

    Refactor main.tsx to wrap BrowserRouter in StrictMode for improved performance and debugging

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,13 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import { AppRouter } from './routes'
 
-
 createRoot(document.getElementById('root')!).render(
-	<BrowserRouter>
-		<AppRouter></AppRouter>
-	</BrowserRouter>,
+	<StrictMode>
+		<BrowserRouter>
+			<AppRouter></AppRouter>
+		</BrowserRouter>
+	</StrictMode>,
 )
